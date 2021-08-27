76c04cc739a07dee4fc0cf91775bc7c5367e8853
fuji114 <fujinuma.1414@gmail.com>
2021-08-27 15:55:51 +0900

    ユーザー登録できるようにした

---
+++ db/migrate/20210827060545_create_movies.rb
@@ -0,0 +1,15 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :movie_title,           null: false
+      t.integer :movie_age_id,           null: false
+      t.integer :film_director
+      t.integer :genre_id,           null: false
+      t.text :synopsis
+      t.integer :movie_rating,           null: false
+      t.text :review
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
